90b87be801fd085edb5828ce788fa82e9ea07083
MmmarRTHa <m27842n@outlook.com>
2023-06-16 23:04:03 -0700

    - update: main - added print function, working on updating the grid.

--- conways_game_of_life/gol.erl
+++ conways_game_of_life/gol.erl
@@ -1,5 +1,5 @@
 -module(gol).
--export([new/2, query/3, assign/4, print/1, step_cell/3, simulate/1, run_simulation/2, demo_grid/0]).
+-export([new/2, query/3, print/1, assign/4,  step_cell/3, simulate/1, run_simulation/2, demo_grid/0]).
 
 -record(grid, {height=10, width=10, rows}).
 -record(transition, {y, x, state}).
@@ -8,9 +8,27 @@ mod(X, Y) when X > 0 -> X rem Y;
 mod(X, Y) when X < 0 -> Y + X rem Y;
 mod(0, _Y) -> 0.
 
-
 new(Height, Width) ->
     Column = array:new(Height),
     Array2d = array:map(fun(_X, _T) -> array:new([{size, Width}, {fixed, true}, {default, empty}]) end, Column),
     #grid{height=Height, width=Width, rows=Array2d}.
-    
+
+query(Grid, Y, X) ->
+    Row = array:get(mod(Y, Grid#grid.height), Grid#grid.rows),
+    array:get(mod(X, Grid#grid.width), Row).
+
+draw(Value) ->
+    case Value of
+        empty -> "-";
+        alive -> "*"
+    end.
+
+print(Grid) ->
+    ColumnIndex = lists:seq(0, Grid#grid.width - 1),
+    RowIndex = lists:seq(0, Grid#grid.height - 1),
+    lists:foreach(fun(Y) ->
+        io:format("|"),
+        lists:foreach(fun(X) ->
+            io:format(" ~s |", [draw(query(Grid, Y, X))]) end, ColumnIndex),
+            io:format("~n") 
+        end, RowIndex).
