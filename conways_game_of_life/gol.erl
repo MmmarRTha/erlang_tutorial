15759aac774eb1f12b42a635ecda0e58cf4c83b1
MmmarRTHa <m27842n@outlook.com>
2023-06-19 16:10:09 -0700

    - update: main - added update grid and demo grid functions.

--- conways_game_of_life/gol.erl
+++ conways_game_of_life/gol.erl
@@ -1,6 +1,7 @@
 -module(gol).
--export([new/2, query/3, print/1, assign/4,  step_cell/3, simulate/1, run_simulation/2, demo_grid/0]).
+-export([new/2, query/3, print/1, assign/4, demo_grid/0]).
 
+% step_cell/3, simulate/1, run_simulation/2,
 -record(grid, {height=10, width=10, rows}).
 -record(transition, {y, x, state}).
 
@@ -17,12 +18,17 @@ query(Grid, Y, X) ->
     Row = array:get(mod(Y, Grid#grid.height), Grid#grid.rows),
     array:get(mod(X, Grid#grid.width), Row).
 
+assign(Grid, Y, X, State) ->
+    Row = array:get(mod(Y, Grid#grid.height), Grid#grid.rows),
+    UpdatedRow = array:set(mod(X, Grid#grid.width), State, Row),
+    Rows = array:set(mod(Y, Grid#grid.height), UpdatedRow, Grid#grid.rows),
+    Grid#grid{rows=Rows}.
+
 draw(Value) ->
     case Value of
         empty -> "-";
         alive -> "*"
     end.
-
 print(Grid) ->
     ColumnIndex = lists:seq(0, Grid#grid.width - 1),
     RowIndex = lists:seq(0, Grid#grid.height - 1),
@@ -32,3 +38,11 @@ print(Grid) ->
             io:format(" ~s |", [draw(query(Grid, Y, X))]) end, ColumnIndex),
             io:format("~n") 
         end, RowIndex).
+
+    demo_grid() ->
+        Grid  = new(5, 9),
+        Grid1 = assign(Grid, 0, 3, alive),
+        Grid2 = assign(Grid1, 1, 4, alive),
+        Grid3 = assign(Grid2, 2, 2, alive),
+        Grid4 = assign(Grid3, 2, 3, alive),
+        assign(Grid4, 2, 4, alive).
